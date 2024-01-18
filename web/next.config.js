c98744fa6defd482f3e85e7957f70aedc5addfbd
Bia Siqueira <developer.biaa@gmail.com>
2024-01-18 04:46:10 -0300

    update

---
+++ web/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'avatars.githubusercontent.com', 'localhost'
+    ]
+  }
+}
+
+module.exports = nextConfig
