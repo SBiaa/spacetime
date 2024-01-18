98ee874d91710662a841869c4d11014ad8b93da6
Bia Siqueira <developer.biaa@gmail.com>
2024-01-18 04:39:57 -0300

    Commitando alterações no submódulo web

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,8 @@
+export function Copyright(){
+  return(
+    <div className="text-sm leading-relaxed text-gray-200">
+    Feito por <a href="" target='_blank' rel='noreferrer' className='underline hover:text-gray-100'>Bia</a>
+   </div>
+  
+  )
+}
